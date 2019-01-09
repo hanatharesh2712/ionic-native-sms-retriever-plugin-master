8e29e6d032e56b85d37b87e01e936b2833abbafd
Haresh Hanat <haresh.hanat@devitpl.com>
2019-01-09 13:34:59 +0530

    changes for calling plugin in js

--- www/smsRetriever.js
+++ www/smsRetriever.js
@@ -1,22 +1,22 @@
 var SmsRetrieverLoader = function(require, exports, module) {
   var exec = require('cordova/exec');
 
-  var intervalId;
+  //var intervalId;
   
   function SmsRetriever() {}
 
   SmsRetriever.prototype.start = function(success, failure, timeOffset) {
     exec(success, failure, 'AndroidSmsRetriever', 'start', []);
-    intervalId = setInterval(function() {
+    setTimeout(function() {
       exec(success, failure, 'AndroidSmsRetriever', 'getCurrent', []);
     }, timeOffset || 500);
   };
 
   SmsRetriever.prototype.stop = function(success, failure) {
-    if (intervalId) {
-      clearInterval(intervalId);
-      intervalId = null;
-    }
+    // if (intervalId) {
+    //   clearInterval(intervalId);
+    //   intervalId = null;
+    // }
     exec(success, failure, 'AndroidSmsRetriever', 'stop', []);
   };
   
